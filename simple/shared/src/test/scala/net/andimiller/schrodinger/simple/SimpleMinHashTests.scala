259bbfaa2befc23f52a5c1803f13d0221d38b1bf
Andi Miller <andi@andimiller.net>
2023-10-28 00:47:05 +0100

    Add jaccard test 🐱❓

--- simple/shared/src/test/scala/net/andimiller/schrodinger/simple/SimpleMinHashTests.scala
+++ simple/shared/src/test/scala/net/andimiller/schrodinger/simple/SimpleMinHashTests.scala
@@ -1,8 +1,10 @@
 package net.andimiller.schrodinger.simple
 
+import cats.data.NonEmptyLazyList
 import cats.kernel.laws.SemilatticeLaws
 import cats.kernel.laws.discipline.SemilatticeTests
 import munit.DisciplineSuite
+import net.andimiller.schrodinger.HasherFactory
 import net.andimiller.schrodinger.simple.arb.SimpleMinHashArbitraries
 
 class SimpleMinHashTests
@@ -14,6 +16,22 @@ class SimpleMinHashTests
     "SimpleMinHash[32]",
     semilattice
   )
+
+  test("Jaccard should give an expected value") {
+    implicit val hasherFactory: HasherFactory[Int, String, Int] =
+      HasherFactory.murmur3
+    val one =
+      SimpleMinHash.fromItems[1024, String](NonEmptyLazyList("hello", "world"))
+    val two =
+      SimpleMinHash.fromItems[1024, String](NonEmptyLazyList("hello"))
+
+    assertEqualsDouble(
+      SimpleMinHash.jaccard(one, two),
+      0.5,
+      delta = 0.03,
+      "Expected jaccard to be around 0.5"
+    )
+  }
 
   override def laws: SemilatticeLaws[SimpleMinHash[32]] =
     SemilatticeLaws[SimpleMinHash[32]]
